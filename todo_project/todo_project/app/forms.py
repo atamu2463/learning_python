b3d5af70821646dac127963ac04cc36150b8cb71
asa-cording <asahiwork.cording@gmail.com>
2026-01-07 16:11:54 +0900

    いったん初期化

--- todo_project/todo_project/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TodoTask
-
-class TodoTaskForm(forms.ModelForm):
-    class Meta:
-        model = TodoTask
-        fields = ['title','description','completed']
